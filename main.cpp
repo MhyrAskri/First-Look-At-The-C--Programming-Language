33c47e024581a245c5263ed92b2ec9514d2fae09
MhyrAskri <askari.m1379@gmail.com>
2021-08-20 15:48:42 +0430

    First programming in C++ language

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int number;
+
+    cout << "Enter your favorite number between 1 and 100: " << endl;
+    cin >> number;
+    cout << "Amazing!! " << number << " is my favorite number too.";
+
+    return 0;
+
+}
